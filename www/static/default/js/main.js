adad263782cf4817446fcc1ecc036903d0f4538a
akira <akira.cn@gmail.com>
2013-11-16 21:22:32 +0800

    update

--- www/static/default/js/main.js
+++ www/static/default/js/main.js
@@ -1,50 +1,18 @@
-(function(define, global) { 'use strict';
-	define(function (require) {
-		function goTop(acceleration, time) {
-			acceleration = acceleration || 0.1;
-			time = time || 16;
+(function() { 'use strict';
+	
+	$.easing.easeOut = function ( p ) {
+		return p * (2 - p);
+	}
 
-			var dx = 0;
-			var dy = 0;
-			var bx = 0;
-			var by = 0;
-			var wx = 0;
-			var wy = 0;
+	function goTop(acceleration, time) {
+		$('body,html').animate({scrollTop:0},{
+			duration: 1000,
+			easing: "easeOut"
+		});
+	}
 
-			if (document.documentElement) {
-				dx = document.documentElement.scrollLeft || 0;
-				dy = document.documentElement.scrollTop || 0;
-			}
-			if (document.body) {
-				bx = document.body.scrollLeft || 0;
-				by = document.body.scrollTop || 0;
-			}
-			var wx = window.scrollX || 0;
-			var wy = window.scrollY || 0;
-
-			var x = Math.max(wx, Math.max(bx, dx));
-			var y = Math.max(wy, Math.max(by, dy));
-
-			var speed = 1 + acceleration;
-			window.scrollTo(Math.floor(x / speed), Math.floor(y / speed));
-			if(x > 0 || y > 0) {
-				setTimeout(function(){
-					goTop(acceleration, time);
-				}, time);
-			}
-		}
-
-		return {
-			goTop : goTop
-		};
-	});
-}) (
-	typeof define === 'function' && define.amd ? define : function (factory) { 
-		if(typeof module != 'undefined'){
-			module.exports = factory(require); 
-		}else if(typeof window != 'undefined'){
-			window.utils = factory();
-		}
-	},
-	this
-);
+	window.utils = {
+		goTop : goTop
+	};
+	
+})();
