38ea9b84895d00930d790cf06d7d9b18272c39e7
akira <akira.cn@gmail.com>
2013-11-21 20:09:36 +0800

    update

--- www/static/default/js/main.js
+++ www/static/default/js/main.js
@@ -1,18 +1,1 @@
-(function() { 'use strict';
-	
-	$.easing.easeOut = function ( p ) {
-		return p * (2 - p);
-	}
-
-	function goTop(acceleration, time) {
-		$('body,html').animate({scrollTop:0},{
-			duration: 1000,
-			easing: "easeOut"
-		});
-	}
-
-	window.utils = {
-		goTop : goTop
-	};
-	
-})();
+console.log('This would be the main JS file.');
